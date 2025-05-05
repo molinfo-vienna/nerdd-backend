b5963f606504ae2da0fff23c8075bdc128501e5d
Steffen Hirte <shirte@users.noreply.github.com>
2025-05-05 14:14:29 +1200

    fix: Handle deleted jobs in SaveResultCheckpointToDb

--- nerdd_backend/actions/save_result_checkpoint_to_db.py
+++ nerdd_backend/actions/save_result_checkpoint_to_db.py
@@ -3,7 +3,7 @@ import logging
 from nerdd_link import Action, Channel, ResultCheckpointMessage, SerializationRequestMessage
 from omegaconf import DictConfig
 
-from ..data import Repository
+from ..data import RecordNotFoundError, Repository
 from ..models import JobUpdate
 
 __all__ = ["SaveResultCheckpointToDb"]
@@ -21,9 +21,16 @@ class SaveResultCheckpointToDb(Action[ResultCheckpointMessage]):
         job_id = message.job_id
         checkpoint_id = message.checkpoint_id
         logger.info(f"Received result checkpoint {checkpoint_id} for job {job_id}")
-        job = await self.repository.update_job(
-            JobUpdate(id=job_id, new_checkpoints_processed=[checkpoint_id])
-        )
+
+        # update job status
+        try:
+            job = await self.repository.update_job(
+                JobUpdate(id=job_id, new_checkpoints_processed=[checkpoint_id], status="processing")
+            )
+        except RecordNotFoundError:
+            # the job might not exist anymore, e.g., if it was deleted
+            logger.warning(f"Job {job_id} not found, skipping checkpoint processing")
+            return
 
         # check if all checkpoints have been processed
         unique_checkpoints = set(job.checkpoints_processed)
