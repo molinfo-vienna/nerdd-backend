4bccd603228947293e404bd585851287096aeab1
Steffen Hirte <shirte@users.noreply.github.com>
2025-05-06 15:49:09 +1200

    fix: Extend constructor of CompressedSet

--- nerdd_backend/util/compressed_set.py
+++ nerdd_backend/util/compressed_set.py
@@ -1,16 +1,33 @@
-from typing import List, Optional, Tuple
+from __future__ import annotations
+
+from typing import List, Optional, Tuple, Union
 
 __all__ = ["CompressedSet"]
 
 
 class CompressedSet:
-    def __init__(self, intervals: Optional[List[Tuple[int, int]]] = None):
-        if intervals is None or len(intervals) == 0:
-            intervals = []
-        # copy the list of intervals
-        self.intervals = list(intervals)
+    def __init__(
+        self, intervals_or_entries: Optional[Union[List[Tuple[int, int]], List[int]]] = None
+    ):
+        if intervals_or_entries is None or len(intervals_or_entries) == 0:
+            intervals_or_entries = []
+        else:
+            if isinstance(intervals_or_entries[0], int):
+                # convert list of entries to list of intervals
+                entries = sorted(set(intervals_or_entries))
+                intervals = []
+                start = entries[0]
+                for i in range(1, len(entries)):
+                    if entries[i] != entries[i - 1] + 1:
+                        intervals.append((start, entries[i - 1] + 1))
+                        start = entries[i]
+                intervals.append((start, entries[-1] + 1))
+                self.intervals = intervals
+            elif isinstance(intervals_or_entries[0], tuple) and len(intervals_or_entries[0]) == 2:
+                # copy the list of intervals
+                self.intervals = list(intervals_or_entries)
 
-    def add(self, x: int) -> "CompressedSet":
+    def add(self, x: int) -> CompressedSet:
         i = 0
         while i < len(self.intervals) and self.intervals[i][0] <= x:
             i += 1
