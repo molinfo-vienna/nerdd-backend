daed8ac5124d9228495cec5c5a8a08810c3a3ce4
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-22 16:28:40 +1200

    fix: Remove source caching (and thus simplify code)

--- nerdd_backend/actions/save_result_to_db.py
+++ nerdd_backend/actions/save_result_to_db.py
@@ -14,39 +14,6 @@ __all__ = ["SaveResultToDb"]
 logger = logging.getLogger(__name__)
 
 
-# function that caches sources in memory
-cache = OrderedDict()
-max_cache_size = 1000
-cache_lock = Lock()
-
-
-async def get_source_by_id(source_id, repository):
-    async with cache_lock:
-        if source_id in cache:
-            # make source_id the most recently used
-            cache.move_to_end(source_id)
-            # return the cached value
-            return cache[source_id]
-
-    # if the source is not in the cache, get it from the database
-    try:
-        source = await repository.get_source_by_id(source_id)
-        filename = source.filename
-    except RecordNotFoundError:
-        filename = source_id
-
-    async with cache_lock:
-        # if the cache is full, remove the least recently used source
-        if len(cache) >= max_cache_size:
-            cache.popitem(last=False)
-
-        # add the new source to the cache and make it the most recently used
-        cache[source_id] = filename
-        cache.move_to_end(source_id)
-
-    return filename
-
-
 class SaveResultToDb(Action[ResultMessage]):
     def __init__(self, channel: Channel, repository: Repository) -> None:
         super().__init__(channel.results_topic(), batch_size=100)
@@ -80,6 +47,20 @@ class SaveResultToDb(Action[ResultMessage]):
         # TODO: check if corresponding modules have correct task types
         # (e.g. "derivative_prediction")
 
+        # we cache sources to minimize database lookups
+        source_cache = {}
+
+        async def _get_source(source_id):
+            if source_id in source_cache:
+                return source_cache[source_id]
+            try:
+                source = await self.repository.get_source_by_id(source_id)
+                filename = source.filename
+            except RecordNotFoundError:
+                filename = source_id
+            source_cache[source_id] = filename
+            return filename
+
         for message in valid_messages:
             job_id = message["job_id"]
 
@@ -92,10 +73,7 @@ class SaveResultToDb(Action[ResultMessage]):
                 and not isinstance(message["source"], str)
             ):
                 translated_sources = await asyncio.gather(
-                    *(
-                        get_source_by_id(source_id, self.repository)
-                        for source_id in message["source"]
-                    )
+                    *(_get_source(source_id) for source_id in message["source"])
                 )
                 message["source"] = [s for s in translated_sources if s is not None]
 
