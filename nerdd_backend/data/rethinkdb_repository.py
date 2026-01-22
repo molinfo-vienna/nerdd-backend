c7a67f030aa5bd6e136efc612bcea6608ec48e7e
Steffen Hirte <shirte@users.noreply.github.com>
2026-01-22 17:42:39 +1300

    fix: Check if record already exists in RethinkDbRepository.create_challenge

--- nerdd_backend/data/rethinkdb_repository.py
+++ nerdd_backend/data/rethinkdb_repository.py
@@ -681,8 +681,12 @@ class RethinkDbRepository(Repository):
             )
         )
 
-        if len(result["changes"]) == 0:
-            raise RecordAlreadyExistsError(Challenge, challenge.id)
+        if result["errors"] > 0:
+            first_error = result.get("first_error", "")
+            if first_error.startswith("Duplicate primary key "):
+                raise RecordAlreadyExistsError(Challenge, challenge.id)
+
+            raise Exception(f"Failed to create challenge: {first_error}")
 
         return Challenge(**result["changes"][0]["new_val"])
 
